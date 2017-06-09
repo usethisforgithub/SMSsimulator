541a1db61c92b660e0f768ff862d0f63e474cd76
alintawang <alinta.wang@gmail.com>
2017-06-09 11:23:47 -0500

    Updated initializing box

--- Driver.java
+++ Driver.java
@@ -8,40 +8,43 @@ import javax.swing.JTextField;
 public class Driver {
 public static void main(String[] args)
 {
-    JTextField aField = new JTextField(5);
+	
+	JTextField aField = new JTextField(5);
     JTextField bField = new JTextField(5);
     JTextField cField = new JTextField(5);
     JTextField dField = new JTextField(5);
-
+    
     JPanel myPanel = new JPanel();
     myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
+    
     myPanel.add(new JLabel("Enter # of rows:"));
     myPanel.add(aField);
-    myPanel.add(Box.createVerticalStrut(15)); 
-
+    
+    myPanel.add(Box.createVerticalStrut(15));
+    
     myPanel.add(new JLabel("Enter # of columns:"));
     myPanel.add(bField);
+
     myPanel.add(Box.createVerticalStrut(15));
     
     myPanel.add(new JLabel("Enter direction of first trajectory (-1, 1):"));
     myPanel.add(cField);
-    myPanel.add(Box.createVerticalStrut(15)); 
 
+    myPanel.add(Box.createVerticalStrut(15));
+    
     myPanel.add(new JLabel("Angle of first drone:"));
     myPanel.add(dField);
-    myPanel.add(Box.createVerticalStrut(15)); 
-    
-    int r = Integer.parseInt(aField.getText());
-    int c = Integer.parseInt(bField.getText());
-    int d = Integer.parseInt(cField.getText());
-    double a = Double.parseDouble(dField.getText());
     
     int result = JOptionPane.showConfirmDialog(null, myPanel, 
-             "Initialize values", JOptionPane.OK_CANCEL_OPTION);
+             "Please Enter X and Y Values", JOptionPane.OK_CANCEL_OPTION);
     if (result == JOptionPane.OK_OPTION) {
     }
     
-    
+	int r = Integer.parseInt(aField.getText());
+	int c = Integer.parseInt(bField.getText());
+	int d = Integer.parseInt(cField.getText());
+	double a = Integer.parseInt(dField.getText());
+
 	ScreenWindow window = new ScreenWindow(r,c,d,a);
 	new Thread(window).start();
 }
