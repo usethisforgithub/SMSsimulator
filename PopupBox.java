a158ecbcc4c16969abc5da487d308b728180bf22
usethisforgithub <usethisforgithub@users.noreply.github.com>
2017-06-09 10:23:33 -0500

    Delete PopupBox.java

--- PopupBox.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class PopupBox{
-	public static void main(String[] args){
-		 String test1= JOptionPane.showInputDialog("Please input mark for test 1: ");
-	} 
-}
