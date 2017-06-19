50f27d9e2993d8bdddbe1826d44ddac1a30cf2f6
Victor Mao <victormao98@gmail.com>
2017-06-19 15:03:20 -0500

    updated JPanel labels
    updated JPanel labels and fixed NumberFormatException

--- Driver.java
+++ Driver.java
@@ -6,52 +6,46 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Driver {
-public static void main(String[] args)
-{
-	//stuff for the dialog box
-	JTextField aField = new JTextField(5);
-    JTextField bField = new JTextField(5);
-    JTextField cField = new JTextField(5);
-    JTextField dField = new JTextField(5);
-    
-    JPanel myPanel = new JPanel();
-    myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-    
-    myPanel.add(new JLabel("Enter # of rows:"));
-    myPanel.add(aField);
-    
-    myPanel.add(Box.createVerticalStrut(15));
-    
-    myPanel.add(new JLabel("Enter # of columns:"));
-    myPanel.add(bField);
+	public static void main(String[] args) {
+		// initialize text boxes
+		JTextField aField = new JTextField(5);
+		JTextField bField = new JTextField(5);
+		JTextField cField = new JTextField(5);
+		JTextField dField = new JTextField(5);
 
-    myPanel.add(Box.createVerticalStrut(15));
-    
-    myPanel.add(new JLabel("Enter direction of first trajectory (-1, 1):"));
-    myPanel.add(cField);
+		JPanel myPanel = new JPanel();
+		myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
 
-    myPanel.add(Box.createVerticalStrut(15));
-    
-    myPanel.add(new JLabel("Angle of first drone:"));
-    myPanel.add(dField);
-    
-    int result = JOptionPane.showConfirmDialog(null, myPanel, 
-             "Please Enter X and Y Values", JOptionPane.OK_CANCEL_OPTION);
-    if (result == JOptionPane.OK_OPTION) {
-    }
-    
-   // String temp1 = aField.getText();
-  //  String temp2 = bField.getText();
-  //  String temp3 = cField.getText();
-  //  String temp4 = dField.getText();
-    
-   // if(r)
-	int r = Integer.parseInt(aField.getText());
-	int c = Integer.parseInt(bField.getText());
-	int d = Integer.parseInt(cField.getText());
-	double a = Double.parseDouble(dField.getText());
+		myPanel.add(new JLabel("Enter number of rows:"));
+		myPanel.add(aField);
 
-	ScreenWindow window = new ScreenWindow(r,c,d,a);
-	new Thread(window).start();
-}
-}
+		myPanel.add(Box.createVerticalStrut(15));
+
+		myPanel.add(new JLabel("Enter number of columns:"));
+		myPanel.add(bField);
+
+		myPanel.add(Box.createVerticalStrut(15));
+
+		myPanel.add(new JLabel("Enter initial direction of first trajectory"));
+		myPanel.add(new JLabel("-1 (clockwise) or 1 (counterclockwise):"));
+		myPanel.add(cField);
+
+		myPanel.add(Box.createVerticalStrut(15));
+
+		myPanel.add(new JLabel("Enter initial angle of first drone"));
+		myPanel.add(new JLabel("in radians between (0.00 - 6.28):"));
+		myPanel.add(dField);
+
+		int result = JOptionPane.showConfirmDialog(null, myPanel, " Start a New SCS Simulation", JOptionPane.OK_CANCEL_OPTION);
+		if (result == JOptionPane.OK_OPTION) {
+			int r = Integer.parseInt(aField.getText());
+			int c = Integer.parseInt(bField.getText());
+			int d = Integer.parseInt(cField.getText());
+			double a = Double.parseDouble(dField.getText());
+
+			ScreenWindow window = new ScreenWindow(r, c, d, a);
+			new Thread(window).start();
+		}
+		
+	} // end Main method
+} // end Driver class
