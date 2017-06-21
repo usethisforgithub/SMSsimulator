6f171f73b5edd933dbfefe72bd59d20d735aea28
usethisforgithub <usethisforgithub@users.noreply.github.com>
2017-06-21 14:17:40 -0500

    some toggles now start as false

--- ScreenWindow.java
+++ ScreenWindow.java
@@ -60,8 +60,8 @@ public class ScreenWindow extends Frame implements WindowListener, Runnable, Key
 		
 		//user inputs
 		paused = true;
-		isolationToggle = true;
-		uncoveringToggle = true;
+		isolationToggle = false;
+		uncoveringToggle = false;
 		ringColorToggle = false;
 		droneLabelToggle = false;
 		numRows = r;
@@ -916,50 +916,7 @@ public class ScreenWindow extends Frame implements WindowListener, Runnable, Key
 			
 			
 			
-			JTextField aField = new JTextField(5);
-		    JTextField bField = new JTextField(5);
-		    JTextField cField = new JTextField(5);
-		    JTextField dField = new JTextField(5);
-		    
-		    JPanel myPanel = new JPanel();
-		    myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-		    
-		    myPanel.add(new JLabel("Enter # of rows:"));
-		    myPanel.add(aField);
-		    
-		    myPanel.add(Box.createVerticalStrut(15));
-		    
-		    myPanel.add(new JLabel("Enter # of columns:"));
-		    myPanel.add(bField);
-
-		    myPanel.add(Box.createVerticalStrut(15));
-		    
-		    myPanel.add(new JLabel("Angle of first drone(Radians 0 - 6.28):"));
-		    myPanel.add(cField);
-
-		    myPanel.add(Box.createVerticalStrut(15));
-		    
-		    myPanel.add(new JLabel("Angle of first drone:"));
-		    myPanel.add(dField);
-		    
-		    int result = JOptionPane.showConfirmDialog(null, myPanel, 
-		             "Please Enter X and Y Values", JOptionPane.OK_CANCEL_OPTION);
-		    if (result == JOptionPane.OK_OPTION) {
-		    }
-		    
-		   // String temp1 = aField.getText();
-		  //  String temp2 = bField.getText();
-		  //  String temp3 = cField.getText();
-		  //  String temp4 = dField.getText();
-		    
-		   // if(r)
-			int r = Integer.parseInt(aField.getText());
-			int c = Integer.parseInt(bField.getText());
-			int d = Integer.parseInt(cField.getText());
-			double a = Double.parseDouble(dField.getText());
-
-			ScreenWindow window = new ScreenWindow(r,c,d,a);
-			new Thread(window).start();
+			System.out.println("coming soon");
 			
 		}
 		
